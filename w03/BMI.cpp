ef32019357aa3557227fbb525a87803e9c9afd19
joejames0223 <43335053+joejames0223@users.noreply.github.com>
2018-09-26 23:33:30 +0800

    Update BMI.cpp

--- w03/BMI.cpp
+++ w03/BMI.cpp
@@ -1,46 +1,37 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
     double H,W,BMI;
-
     printf("How tall are you?(cm)\n");
     scanf("%lf",&H);
-
     printf("How much do you weigh?(kg)\n");
     scanf("%lf",&W);
-
     BMI=W/((H/100)*(H/100));
-    
-    while(H>=0 && W>=0){
+while(W>=0&&H>=0){
+    printf("你的BMI為: %.1lf \n",BMI);
     if(BMI<18.5){
-    	printf("你的BMI為: %.1lf \n",BMI);
-        printf("體重過輕\n");
+         printf("體重過輕\n");
     }
-    if(BMI>=18.5&&BMI<24){
-    	printf("你的BMI為: %.1lf \n",BMI);
-        printf("正常範圍\n");
+    else if(BMI>=18.5&&BMI<24){
+         printf("正常範圍\n");
     }
-    if(BMI>=24&&BMI<27){
-    	printf("你的BMI為: %.1lf \n",BMI);
-        printf("體重過重\n");
+    else if(BMI>=24&&BMI<27){
+         printf("體重過重\n");
     }
-    if(BMI>=27&&BMI<30){
-    	printf("你的BMI為: %.1lf \n",BMI);
-        printf("輕度肥胖\n");
+    else if(BMI>=27&&BMI<30){
+         printf("輕度肥胖\n");
     }
-    if(BMI>=30&&BMI<35){
-    	printf("你的BMI為: %.1lf \n",BMI);
+    else if(BMI>=30&&BMI<35){
         printf("中度肥胖\n");
     }
-    if(BMI>35){
-    	printf("你的BMI為: %.1lf \n",BMI);
+    else if(BMI>=35){   //>=
         printf("重度肥胖\n");
+}
     printf("你的身高:");
     scanf("%lf",&H);
     printf("你的體重:");
     scanf("%lf",&W);
-    }
+    BMI=W/((H/100)*(H/100)); //就差這一行 
 }
-    return 0;
 }
+
