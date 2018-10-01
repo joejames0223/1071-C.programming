d58adaba7f08fc5ca8abb4c29d1ad3ab0640e8fe
joejames0223 <43335053+joejames0223@users.noreply.github.com>
2018-10-01 18:01:13 +0800

    Create menu.cpp

---
+++ w03/menu.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(){
+
+    int choice;    //用到哪，宣告到哪
+    int height;
+    int width;
+    int type;
+
+    while(1){
+
+    printf("Main menu\n");
+    printf("1. Retangle\n");
+    printf("2. Vertical Triangle\n");
+    printf("3. Exit\n");
+    printf("=> ");
+    scanf("%d", &choice);
+    if(choice==3)break;
+    switch(choice)
+    {
+    case 1:
+        printf("Enter height and width: ");
+        scanf("%d %d", &height , &width);  //%d跟%d之間可以視情況加","
+        for(int i=1;i<=height;i++){
+            for(int j=1;j<=width;j++){
+                if(i==1 || i==height || j==1 || j==width)
+                        printf("*");
+
+                    else
+                        printf(" ");
+            }
+            printf("\n");
+        }
+        break;
+    case 2:
+        printf("Enter height and type: \n");
+        scanf("%d %d", &height , &type);
+        for(int i=1;i<=height;i++){
+            for(int j=1;j<=ype;j++){
+                if(i==1 || i==height || j==1 || j==width)
+
+        }
+
+
+
+
+
+        break;
+
+}
+        }
+}
+
