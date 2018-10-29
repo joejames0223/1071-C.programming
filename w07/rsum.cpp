85c41c2962294aaabf8e19675b155b15df150ca8
joejames0223 <43335053+joejames0223@users.noreply.github.com>
2018-10-29 20:07:54 +0800

    Create rsum.cpp

---
+++ w07/rsum.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+ //sum(n) = 1+2+3+...(n+1)+n
+ int sum(int n){
+     int total=0;
+     for(int i=1;i<=n;i++){
+        total += i;
+     }
+     return total;
+ }
+
+ int rsum(int n){
+     printf("%d\n", n);
+     if(n==1)
+        return 1;
+     else
+        return rsum(n-1)+n;
+}
+
+int main(){
+    int n;
+    printf("Enter n:");           //加入這一行即可
+    scanf("%d", &n);
+    printf("sum(%d)=%d\n",n,sum(n));
+    printf("rsum(%d)=%d\n",n,rsum(n));
+}
