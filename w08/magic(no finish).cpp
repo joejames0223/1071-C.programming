ca2761c8be03ee64f5d13ba9cc447e81d1e6cec1
joejames0223 <43335053+joejames0223@users.noreply.github.com>
2018-10-29 17:55:02 +0800

    Create magic(no finish).cpp

---
+++ w08/magic(no finish).cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define MAX 50
+
+void prArr(int m[][MAX], int N){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            printf("%3d ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void magic(int m[][MAX], int N){
+    int row=N-1;
+    int col=N-2;
+    m[row][col] = 1;
+    for(int i=2;i<=N*N;i++){
+     //find next row, col
+     if(i%N==1){
+
+     }else{
+
+     }
+     m[row][col] = i;
+    }
+}
+int main(){
+    int m[MAX][MAX]={0};
+    int N=5;
+    prArr(m,N);
+}
+/*Diagonal Move
+ 11  18  25   2   9
+ 10  12  19  21   3
+  4   6  13  20  22
+ 23   5   7  14  16
+ 17  24   1   8  15
+
+Knight Move
+ 17   6  25  14   3
+ 11   5  19   8  22
+ 10  24  13   2  16
+  4  18   7  21  15
+ 23  12   1  20   9*/
